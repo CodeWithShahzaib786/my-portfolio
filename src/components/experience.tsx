5d2f878cbe4212181d35eb6c9e455c69bda1838f
CodeWithShahzaib786 <shahzaibkhaimkhani@gmail.com>
2025-03-29 00:38:26 +0500

    second commit

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,51 +1,62 @@
-import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 
 const Experience = () => {
   // My Experience here
-  const experience = [
-    {
-      experience: "Project-Based Experience",
-      work: [
-        "Developed multiple projects using TypeScript, Next.js, Tailwind CSS, Python, HTML, and CSS",
-        "Built real-world applications and hackathons including an ATM System, a Currency Converter, Nike Ecommerce Website and a Resume Builder.",
-        "Gained experience in API integration, UI/UX design, and web development best practices.",
-      ],
-    },
-  ];
+    const experience = [
+        {
+            companyName : "Artistic Milliners (AM-4)",
+            dateStart : "Sep 2024",
+            dateEnd : "Oct 2024",
+           position : "Internee",
+            work : ["I completed 4 week internship program in cut to pack process"]
+        },
+      
+    ]
   return (
-    <Card data-aos="zoom-in" className="h-[85vh] overflow-y-auto">
-      <CardHeader>
-        <CardTitle className="text-2xl font-semibold tracking-tight uppercase">
-          Experience
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <div className="flex flex-col items-center justify-center -my-8 divide-y-2 divide-gray-100">
-          {/* Graduation */}
-          {experience.map((item, i) => (
-            <div
-              key={i}
-              className="py-8 flex flex-wrap md:flex-nowrap justify-center"
-            >
-              <div className="md:flex-grow text-center">
-                {" "}
-                {/* Center Text */}
+    <Card data-aos="zoom-in" className='h-[85vh] overflow-y-auto'>
+    <CardHeader>
+      <CardTitle className='text-2xl font-semibold tracking-tight uppercase'>Experience</CardTitle>
+    </CardHeader>
+    <CardContent className="space-y-2">
+    <div className="-my-8 divide-y-2 divide-gray-100">
+        {/* Graduation */}
+        {experience.map((item, i)=> (
+              <div key={i} className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-gray-700 uppercase">
+                  {item.position}
+                </span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  {`${item.dateStart} - ${item.dateEnd}`}
+                </span>
+              </div>
+              <div className="md:flex-grow">
                 <h2 className="text-xl font-medium text-gray-900 title-font mb-2 uppercase">
-                  {item.experience}
+                  {item.companyName}
                 </h2>
-                <ul className="ml-4 list-disc mr-2 text-left">
-                  {item.work.map((workItem, i) => (
+                <ul className='ml-4 list-disc mr-2'>
+                  {item.work.map((workItem,i)=>(
                     <li key={i}>{workItem}</li>
                   ))}
+                
                 </ul>
               </div>
             </div>
-          ))}
-        </div>
-      </CardContent>
-    </Card>
-  );
-};
+        ))}
+    </div>
 
-export default Experience;
+
+    </CardContent>
+  </Card>
+  )
+}
+
+export default Experience
+
